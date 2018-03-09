1eec8b3df663f6d0a511f488746b222fdadead07
Jeff Ong <jeffrey.ong@frogdesign.com>
2018-03-09 15:15:41 -0500

    rename

---
+++ 09_3/rename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# a small program that renames a given
+# echo -n "Enter a directory to rename jpegs and press [ENTER]: "
+# read name
+
+# echo $(cd $name && rename -n 's/.*jpeg/new_$1/' *)
+
+# echo "Positional Parameters"
+# echo '$0 = ' $0
+# echo '$1 = ' $1
+# echo '$2 = ' $2
+# echo '$3 = ' $3
+# directory=$1
+# echo '$1 =' $1
+
+# rename -n 's/.*jpeg/new_$1/' *
