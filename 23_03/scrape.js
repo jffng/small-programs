fd7bbe161eeaf2e59e93df708465d7be84895702
Jeff Ong <jeffrey.ong@frogdesign.com>
2018-03-23 18:41:43 -0400

    load the cookies, url

--- 23_03/scrape.js
+++ 23_03/scrape.js
@@ -1,7 +1,22 @@
 const puppeteer = require('puppeteer');
+const config = require('../config').puppeteer;
 
 (async() => {
   const browser = await puppeteer.launch();
-  console.log(await browser.version());
+  const page = await browser.newPage();
+
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36')
+  await page.setCookie({
+    name: config['cookie_name'],
+    value: config['cookie_value'],
+    domain: config['cookie_domain'] 
+  });
+  
+  await page.goto(config['url']);
+  await page.screenshot({ path: 'screenshot.png' });
+  const results = await page.$('#experience-section');
+  console.log(results)
+
+  await page.close();
   await browser.close();
 })();
